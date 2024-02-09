64ae62555293bb0babd15377fe2350df6b586459
Maximilian Temeschinko <maxiteme@peoplesmarkets.com>
2024-02-09 09:26:34 +0100

    little fix in crystal roi table

--- src/pages/roi/crystals.tsx
+++ src/pages/roi/crystals.tsx
@@ -63,15 +63,16 @@ function ViewCrystalRoiRow(props: RowProps) {
     const eleSolPrice = useEleSolPriceStore((state) => state.price);
 
     const [solPerDay, setSolPerDay] = useState(0);
-    const [fpInSol, setFpInSol] = useState(0);
-    const [roi, setRoi] = useState(0);
+    const [fpInSol, setFpInSol] = useState<number>();
+    const [roi, setRoi] = useState<number>();
 
     useEffect(() => {
+        const perDay = calculatePrice(eleSolPrice, CRYSTALS_ELE_PER_HOUR[props.level] * 24);
+        setSolPerDay(perDay);
+
         if (!_.isNil(props.price) && !_.isNil(eleSolPrice)) {
-            const perDay = calculatePrice(eleSolPrice, CRYSTALS_ELE_PER_HOUR[props.level] * 24);
             const fp = calculatePrice(1, props.price);
 
-            setSolPerDay(perDay);
             setFpInSol(fp);
             setRoi(_.round(fp / perDay, 2));
         }
@@ -82,8 +83,8 @@ function ViewCrystalRoiRow(props: RowProps) {
             <TableCell>{props.level}</TableCell>
             <TableCell>{CRYSTALS_ELE_PER_HOUR[props.level]} ELE/h</TableCell>
             <TableCell>{solPerDay} SOL/d</TableCell>
-            <TableCell>{fpInSol} SOL</TableCell>
-            <TableCell>{roi} days</TableCell>
+            <TableCell>{fpInSol ? fpInSol + ' SOL' : 'unknown'}</TableCell>
+            <TableCell>{roi ? roi + ' days' : 'unknown'}</TableCell>
         </TableRow>
     );
 }
